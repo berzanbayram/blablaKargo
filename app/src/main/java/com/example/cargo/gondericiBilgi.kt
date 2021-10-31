63860e4c843e736551b890803020707d532736c1
Berzan Bayram <75726793+berzanbayram@users.noreply.github.com>
2021-10-31 20:29:15 +0300

    updatedd

---
+++ app/src/main/java/com/example/cargo/gondericiBilgi.kt
@@ -0,0 +1,17 @@
+package com.example.cargo
+
+class gondericiBilgi {
+    var GondericiAdSoyad : String = ""
+    var Gondericiilce : String = ""
+    var Gondericimahalle : String = ""
+    var Gondericisokak : String = ""
+
+    constructor(GondericiAdSoyad:String,ilce:String,mahalle:String,sokak:String){
+        this.GondericiAdSoyad=GondericiAdSoyad
+        this.Gondericiilce=ilce
+        this.Gondericimahalle=mahalle
+        this.Gondericisokak=sokak
+
+
+    }
+}
